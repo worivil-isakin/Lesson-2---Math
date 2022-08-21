551deb572b094864f00984cb0234360c3eca61eb
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2022-08-21 05:10:05 -0400

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,53 @@
+// Adding in Javascript
+
+let x = 10;
+let y = 5;
+
+let add = x + y;
+
+console.log("Adding in Javascript");
+console.log(add);
+
+// Subtraction in Javascript
+
+let subtract = x - y;
+console.log("Subtraction in Javascript");
+console.log(subtract);
+
+// Multiplication in Javascript
+let Multiplication = x * y
+console.log("Multiplication in Javascript");
+console.log(Multiplication);
+
+// To find the power in javascript
+let power = x ** 2 
+console.log("To find the power in javascript");
+console.log(power);
+let power2 = Math.pow(x, 2);
+console.log(power2);
+
+// Divison in Javascript
+let divison = x / y;
+console.log("Divison in Javascript");
+console.log(divison);
+
+// To get the Remainder or Modulus in Javascript
+let remainder = x % 3;
+console.log("Remainder in Javascript")
+console.log(remainder)
+
+// Increment in Javascript
+let i = 1;
+
+i++; 
+console.log("Increase by 1");
+console.log(i)
+
+// decrement in Javascript
+let d = 5
+console.log("decrease by 1")
+d--;
+console.log(d)
+
+
+
